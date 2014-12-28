c7c3ded24e520cb125451d2bffcefa8c6f3b0d2d
Yohendry Hurtado <yohendry.hurtado@gmail.com>
2014-12-28 13:27:01 -0500

    bad pins at example

--- examples/test_drive/test_drive.ino
+++ examples/test_drive/test_drive.ino
@@ -1,6 +1,6 @@
 #include <L298N.h>
 
-L298N motors(6,8,7,2,3,4); //motors(ENA,IN1,IN2,IN3,IN4,ENB)
+L298N motors(6,8,7,2,4,3); //motors(ENA,IN1,IN2,IN3,IN4,ENB)
 int time_delay = 200;
 int speed = 150;
 void setup()
