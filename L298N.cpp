1ea118778ba2ceeab655280e08ad621f86fb07cd
Yohendry Hurtado <yohendry.hurtado@gmail.com>
2014-12-29 22:11:40 -0500

    now we can control individual motors

--- L298N.cpp
+++ L298N.cpp
@@ -7,6 +7,14 @@ Released into the public domain.
 #include "Arduino.h"
 #include "L298N.h"
 
+struct Motor {
+  int in1;
+  int in2;
+  int pwn;
+};
+
+Motor motors[2];
+
 L298N::L298N(int ena, int in1, int in2, int in3, int in4, int enb) {
   pinMode (ena, OUTPUT);
   pinMode (in1, OUTPUT);
@@ -14,49 +22,62 @@ L298N::L298N(int ena, int in1, int in2, int in3, int in4, int enb) {
   pinMode (in3, OUTPUT);
   pinMode (in4, OUTPUT);
   pinMode (enb, OUTPUT);
-  _ena = ena;
-  _in1 = in1;
-  _in2 = in2;
-  _in3 = in3;
-  _in4 = in4;
-  _enb = enb;
+
+  motors[0].in1 = in1;
+  motors[0].in2 = in2;
+  motors[0].pwn = ena;
+
+  motors[1].in1 = in3;
+  motors[1].in2 = in4;
+  motors[1].pwn = enb;
 }
 
-void L298N::drive_motors(int speed, int delay_time) {
-  analogWrite(_ena,speed);
-  analogWrite(_enb,speed);
-  delay(delay_time);
+void L298N::drive_motors(int speed) {
+  this->drive_motor(this->MOTOR_A,speed);
+  this->drive_motor(this->MOTOR_B,speed);
+}
+
+void L298N::drive_motor(int motor_index, int speed) {
+  analogWrite(motors[motor_index].pwn,speed);
 }
 
 void L298N::forward(int speed, int delay_time) {
   this->setup_motors(HIGH,LOW,HIGH,LOW);
-  this->drive_motors(speed,delay_time);
+  this->drive_motors(speed);
+  delay(delay_time);
 }
 
 void L298N::turn_right(int speed, int delay_time) {
   this->setup_motors(LOW,HIGH,HIGH,LOW);
-  this->drive_motors(speed,delay_time);
+  this->drive_motors(speed);
+  delay(delay_time);
 }
 
 void L298N::turn_left(int speed, int delay_time) {
   this->setup_motors(HIGH,LOW,LOW,HIGH);
-  this->drive_motors(speed,delay_time);
+  this->drive_motors(speed);
+  delay(delay_time);
 }
 
 void L298N::backward(int speed, int delay_time) {
   this->setup_motors(LOW,HIGH,LOW,HIGH);
-  this->drive_motors(speed,delay_time);
+  this->drive_motors(speed);
+  delay(delay_time);
 }
 
 void L298N::full_stop(int delay_time) {
   this->setup_motors(LOW,LOW,LOW,LOW);
-  this->drive_motors(0,delay_time);
+  this->drive_motors(0);
+  delay(delay_time);
 }
 
 
 void L298N::setup_motors(int state1, int state2, int state3, int state4) {
-  digitalWrite (_in1, state1);
-  digitalWrite (_in2, state2);
-  digitalWrite (_in3, state3);
-  digitalWrite (_in4, state4);
+  L298N::setup_motor(this->MOTOR_A,state1,state2);
+  L298N::setup_motor(this->MOTOR_B,state3,state4);
+}
+
+void L298N::setup_motor(int motor_index, int state1, int state2) {
+  digitalWrite(motors[motor_index].in1, state1);
+  digitalWrite(motors[motor_index].in2, state2);
 }
--- L298N.h
+++ L298N.h
@@ -11,15 +11,20 @@
 class L298N
 {
   public:
+    static const int MOTOR_A = 0;
+    static const int MOTOR_B = 1;
     L298N(int ena, int in1, int in2, int in3, int in4, int enb);
-    void drive_motors(int speed, int delay_time);
+    void drive_motors(int speed);
+    void drive_motor(int motor_index, int speed);
     void setup_motors(int state1, int state2, int state3, int state4);
+    void setup_motor(int motor_index, int state1, int state2);
     void forward(int speed, int delay_time);
     void full_stop(int delay_time);
     void turn_right(int speed, int delay_time);
     void turn_left(int speed, int delay_time);
     void backward(int speed, int delay_time);
   private:
+    struct Motor;
     int _ena;
     int _in1;
     int _in2;
